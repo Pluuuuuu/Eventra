92d40fadffa5aff6778b63412bb53f79d51a374f
mary <maryamzienaldeeen@gmail.com>
2025-08-01 10:47:00 +0300

    auto

---
+++ run-with-hot-reload.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Starting Eventra App with Hot Reload Support...
+echo.
+echo Make sure you have the Java Extension Pack installed in VS Code
+echo and the java.debug.settings.hotCodeReplace is set to "auto"
+echo.
+echo To start debugging with hot reload:
+echo 1. Open VS Code
+echo 2. Press F5 or go to Run and Debug
+echo 3. Select "Launch Eventra App (Debug)"
+echo 4. Make changes to your code and save
+echo 5. Changes will be automatically reloaded!
+echo.
+pause 
